c7e103aeb16af86757e8b5acc12599d21caf3769
Dorob7 <167528640+Dorob7@users.noreply.github.com>
2024-05-17 17:36:37 +0200

    Java.js

--- Java.js
+++ Java.js
@@ -8,13 +8,13 @@ var day = currentDate.getDate().toString().padStart(2, "0");
 var formattedDate = day + "-" + month + "-" + year;
 
 const emailIND =
-  "adrian.sedziak@cevalogistics.com,agnieszka.nowak@cevalogistics.com,ali.sahin@cevalogistics.com,bartosz.jamrozy@cevalogistics.com,jasper.schuller@cevalogistics.com,mo.el.mrabet@cevalogistics.com,haris.duratovic@cevalogistics.com,ellen.dumoulin@cevalogistics.com,roberta.katiliute@cevalogistics.com,magdalena.siegert@cevalogistics.com,edwin.van.vugt@cevalogistics.com,imnl.security.desk.industrieweg@cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
-const emailVW = "vw.beveiliging@bol.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
-const emailBFC = "bfc.beveiliging@bol.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
-const emailBRC = "brc.beveiliging@bol.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
+  "adrian.sedziak@cevalogistics.com,agnieszka.nowak@cevalogistics.com,ali.sahin@cevalogistics.com,bartosz.jamrozy@cevalogistics.com,jasper.schuller@cevalogistics.com,mo.el.mrabet@cevalogistics.com,haris.duratovic@cevalogistics.com,ellen.dumoulin@cevalogistics.com,roberta.katiliute@cevalogistics.com,magdalena.siegert@cevalogistics.com,edwin.van.vugt@cevalogistics.com,imnl.security.desk.industrieweg@cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com,Joanna.RasPetraitis@Cevalogistics.com,arnas.bulatovas@cevalogistics.com";
+const emailVW = "vw.beveiliging@bol.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com,Joanna.RasPetraitis@Cevalogistics.com,arnas.bulatovas@cevalogistics.com";
+const emailBFC = "bfc.beveiliging@bol.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com,Joanna.RasPetraitis@Cevalogistics.com,arnas.bulatovas@cevalogistics.com";
+const emailBRC = "brc.beveiliging@bol.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com,Joanna.RasPetraitis@Cevalogistics.com,arnas.bulatovas@cevalogistics.com";
 const emailBSE = "sh-gl-security.bestseller@cevalogistics.com,anna.romaniuk@cevalogistics.com,patrycja.papina@cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
-const wfpe = "SH-EU-CLS-BOL-Workforceplanning@Cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
-const wfpb = "SH-EU-CLS-Workforceplanning@cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com";
+const wfpe = "SH-EU-CLS-BOL-Workforceplanning@Cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com,Joanna.RasPetraitis@Cevalogistics.com,arnas.bulatovas@cevalogistics.com";
+const wfpb = "SH-EU-CLS-Workforceplanning@cevalogistics.com?cc=sh-eu-nl-cls-flex.coaches@cevalogistics.com,Joanna.RasPetraitis@Cevalogistics.com,arnas.bulatovas@cevalogistics.com";
 
 const ind = document.getElementById("ind");
 const vw = document.getElementById("vw");
@@ -193,6 +193,47 @@ function sendEmailWFP(buttonId) {
     "&subject=" +
     encodeURIComponent(building + " - Name - Agency - Issue") +
     "&body=Good%20day%20Workforce%20planning,%0D%0A%0D%0Aplease%20check%20information%20below:%0D%0A%0D%0A1.Name:XXXXXX%0D%0A%0D%0A2.Agency:%20XXXXXX%0D%0A%0D%0A3.Date%20of%20contact:%20XXXX%0D%0A%0D%0A4.Prior%20contact%20with%20employer:%20yes/no%0D%0A%0D%0A5.Feedback%20from%20employer%20:%20yes/no%0D%0A%0D%0A6.Category%20and%20short%20description:%20Transport/Planning/Skills%20in%20Isabel/Housing/Contact%20with%20employer/Contracting%20&%20documents/Holiday%20request/Payslip%20and%20salary/Cross-training/Health%20issues/Work%20hours%20registration/Access%20card%20and%20facility/Outflow%20request/No-show%20registration,%20personal%20issues/problems%0D%0A%0D%0A7.Advice%20of%20action%20to%20be%20taken:%20contact%20employer/Check%20planning%20possibilities%20(includes%20extra%20planning,%20crosstraining)/Contact%20operations/Resolve%20internally/Outflow%0D%0A%0D%0A8.Attachments:%20yes/no%0D%0A%0D%0AThank%20you%20for%20processing%20this%20request";
+  window.location.href = emailLink;
+}
 
+function sendEmailInstroom(buttonId) {
+  var year = currentDate.getFullYear();
+  var month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+  var day = (currentDate.getDate() - 1).toString().padStart(2, "0");
+  var date = day + "-" + month + "-" + year;
+
+  let building = "";
+  let email = "";
+
+  switch (buttonId) {
+    case "vw":
+      building = "VW";
+      email = wfpe;
+      break;
+    case "ind":
+      building = "IND";
+      email = wfpb;
+      break;
+    case "bfc":
+      building = "BFC";
+      email = wfpe;
+      break;
+    case "brc":
+      building = "BRC";
+      email = wfpe;
+      break;
+    case "bse":
+      building = "BSE";
+      email = wfpb;
+    default:
+      break;
+  }
+
+  var emailLink =
+    "mailto:" +
+    email +
+    "?subject=" +
+    encodeURIComponent(building + " - Instroom Update " + date) +
+    "&body=Good day,%0D%0A%0D%0ABelow you will find update for the new instroom%0D%0A%0D%0A[Additional Info]";
   window.location.href = emailLink;
 }
